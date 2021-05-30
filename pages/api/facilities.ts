cee8f359c6d019def9dae21348833c0e272557cb
Pawel Nackiewicz <it.pnackiewicz@gmail.com>
2021-05-30 22:23:10 +0200

    :seedling: improve API

--- pages/api/facilities.ts
+++ pages/api/facilities.ts
@@ -11,11 +11,11 @@ const Facilities: Facility[] = [
     streetName: 'Wielka',
     city: 'Opole',
     houseNumber: '3',
-    postcode: '21-221',
+    postCode: '21-221',
     description:
       'najlepsze warzywa w okolicynajlepsze warzywa w okolicynajlepsze warzywa w okolicynajlepsze warzywa w okolicy najlepsze warzywa w okolicy najlepsze warzywa w okolicy... najlepsze warzywa w okolicy... najlepsze warzywa w okolicy.... najlepsze warzywa w okolicy...',
-    latitude: 17.188637,
-    longitude: 50.170789,
+    latitude: 17.88068,
+    longitude: 50.66825,
   },
   {
     id: '600b033e7d246d1a4c8a49b5',
@@ -24,11 +24,11 @@ const Facilities: Facility[] = [
     streetName: 'Wielka',
     city: 'Opole',
     houseNumber: '3',
-    postcode: '21-221',
+    postCode: '21-221',
     description:
       'najlepsze warzywa w okolicynajlepsze warzywa w okolicynajlepsze warzywa w okolicynajlepsze warzywa w okolicy najlepsze warzywa w okolicy najlepsze warzywa w okolicy... najlepsze warzywa w okolicy... najlepsze warzywa w okolicy.... najlepsze warzywa w okolicy...',
-    latitude: 17.188631,
-    longitude: 53.170781,
+    latitude: 18.151939,
+    longitude: 50.702629,
   },
   {
     id: '600b033e7d246d1a4c8a49b6',
@@ -37,11 +37,11 @@ const Facilities: Facility[] = [
     streetName: 'Wielka',
     city: 'Opole',
     houseNumber: '3',
-    postcode: '21-221',
+    postCode: '21-221',
     description:
       'najlepsze warzywa w okolicynajlepsze warzywa w okolicynajlepsze warzywa w okolicynajlepsze warzywa w okolicy najlepsze warzywa w okolicy najlepsze warzywa w okolicy... najlepsze warzywa w okolicy... najlepsze warzywa w okolicy.... najlepsze warzywa w okolicy...',
-    latitude: 11.188677,
-    longitude: 50.170789,
+    latitude: 18.04219,
+    longitude: 50.66663,
   },
   {
     id: '600b033e7d246d1a4c8a49b7',
@@ -50,11 +50,11 @@ const Facilities: Facility[] = [
     streetName: 'Wielka',
     city: 'Opole',
     houseNumber: '3',
-    postcode: '21-221',
+    postCode: '21-221',
     description:
       'najlepsze warzywa w okolicynajlepsze warzywa w okolicynajlepsze warzywa w okolicynajlepsze warzywa w okolicy najlepsze warzywa w okolicy najlepsze warzywa w okolicy... najlepsze warzywa w okolicy... najlepsze warzywa w okolicy.... najlepsze warzywa w okolicy...',
-    latitude: 12.188637,
-    longitude: 50.170769,
+    latitude: 17.96933,
+    longitude: 50.67567,
   },
 ];
 
