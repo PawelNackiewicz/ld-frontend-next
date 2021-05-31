17ed0d4b502b15f9fed4ca828cd4bed8307ad3b4
Pawel Nackiewicz <it.pnackiewicz@gmail.com>
2021-05-31 20:58:38 +0200

    :sparkles: add dolacz do projektu description

--- pages/dolacz_do_projektu.tsx
+++ pages/dolacz_do_projektu.tsx
@@ -1,11 +1,34 @@
 import Image from 'next/image';
 import Layout from '../components/Layouts/Layouts';
+import styles from './styles/dolacz_do_projektu.module.scss';
 
 export default function AboutProject() {
   return (
     <Layout>
-      <div>
-        <h1>Hello dolacz do projektu</h1>
+      <div className={styles.pageContainer}>
+        <h1>Jak dołączyć do projektu?</h1>
+        <article>
+          Projekt jest skierowany do osób które posiadają <strong>własne produkty</strong>.<br />{' '}
+          Dołaczenie do projektu jest <strong>całkowicie darmowe</strong>.<br /> Aby dołączyć do
+          projektu wystarczy wysłać maila na adres{' '}
+          <a href="mailto:lokalne-dobrodziejstwa@gmail.com">lokalne-dobrodziejstwa@gmail.com</a>{' '}
+          <br />z niezbędnymi informacjami.{' '}
+        </article>
+        <div>
+          <p>Do tych informacji należą:</p>
+          <ul>
+            <li>Dane osoby kontaktowej - Imie nazwisko oraz email</li>
+            <li>Nazwa firmy (jeśli istnieje)</li>
+            <li>Informacje o ofercie</li>
+            <li>Zdjęcie produktów oraz (ewentualnie) logo firmy</li>
+            <li>Informacje adresowe o placówkach gdzie można zakupić produkty </li>
+            <li>Adres strony internetowe/lub innych social mediów (jeśli istnieją)</li>
+          </ul>
+          <p>
+            Pamiętaj, że wysyłając maila z takimi informacjami{' '}
+            <strong>zgadzasz się na przetwarzanie ww danych</strong>.{' '}
+          </p>
+        </div>
         <Image alt="apple" src="/animations/apple.svg" width={160} height={160} />
       </div>
     </Layout>
--- pages/o_projekcie.tsx
+++ pages/o_projekcie.tsx
@@ -5,7 +5,7 @@ export default function AboutProject() {
   return (
     <Layout>
       <div>
-        <h1>Hello About Project</h1>
+        <h1>O Projekcie</h1>
         <Image alt="apple" src="/animations/apple.svg" width={160} height={160} />
       </div>
     </Layout>
